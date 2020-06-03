d1a1426d379895da6b0f6f8e1a0f5c6712ba980a
Patrick Deutschmann <patrick.deutschmann@student.tugraz.at>
2020-06-03 22:32:26 +0200

    Add different inputs to network, include dropout layers

--- aa.py
+++ aa.py
@@ -2,6 +2,7 @@ from aa_data_prep import prepare_data
 from aa_classifier import AuthorClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
+import tensorflow as tf
 
 
 def split_train_test_val(X, test_split, val_split, rand_seed):
@@ -10,6 +11,18 @@ def split_train_test_val(X, test_split, val_split, rand_seed):
     X_train, X_val \
         = train_test_split(X_train, test_size=val_split / (1 - test_split), random_state=rand_seed)
     return X_train, X_test, X_val
+
+
+def prepare_input(embedded_posts, date_stats, article_stats, num_article_category_1, num_article_category_2):
+    rnn_inputs = embedded_posts
+
+    date_inputs = np.asarray(date_stats.drop("ID_Post", axis=1).drop("Timestamp", axis=1))
+    article_inputs = np.hstack([tf.keras.utils.to_categorical(article_stats["ArticleCategory1"].cat.codes,
+                                                              num_classes=num_article_category_1),
+                                tf.keras.utils.to_categorical(article_stats["ArticleCategory2"].cat.codes,
+                                                              num_classes=num_article_category_2)])
+    dense_inputs = np.hstack([date_inputs, article_inputs])
+    return {"rnn": np.asarray(rnn_inputs), "dense": np.asarray(dense_inputs)}
 
 
 if __name__ == '__main__':
@@ -31,8 +44,17 @@ if __name__ == '__main__':
         = split_train_test_val(targets, test_split, val_split, rand_seed)
 
     # TODO try different hyper-parameters using sklearn.model_selection
-    classifier = AuthorClassifier(epochs=50)
-    classifier.fit(embedded_posts_train, targets_train)
+    classifier = AuthorClassifier(epochs=100, dropout=0.15)
 
-    test_score = classifier.score(embedded_posts_test, targets_test)
+    num_article_category_1 = np.max(article_stats["ArticleCategory1"].cat.codes) + 1
+    num_article_category_2 = np.max(article_stats["ArticleCategory2"].cat.codes) + 1
+
+    X_train = prepare_input(embedded_posts_train, date_stats_train, article_stats_train,
+                            num_article_category_1, num_article_category_2)
+    X_val = prepare_input(embedded_posts_val, date_stats_val, article_stats_val,
+                            num_article_category_1, num_article_category_2)
+    classifier.fit(X_train, targets_train, X_val, targets_val)
+
+    X_test = prepare_input(embedded_posts_test, date_stats_test, article_stats_test)
+    test_score = classifier.score(X_test, targets_test)
     print("Test score = " + str(test_score))
--- aa_classifier.py
+++ aa_classifier.py
@@ -20,17 +20,20 @@ class AuthorClassifier(BaseEstimator, ClassifierMixin):
         self._model = None
         self._num_users = None
         self._num_posts = None
+        self._num_dense_inputs = None
         self._post_max_length = None
         self._post_embedding_dimension = None
 
-    def fit(self, X, Y):
-        assert X.shape[0] == Y.shape[0], "number of posts in input and output must match"
+    def fit(self, X, Y, X_val, Y_val):
+        assert X["dense"].shape[0] == X["rnn"].shape[0] == Y.shape[0], "number of posts in inputs and output must match"
 
         self._num_users = Y.shape[1]
-        self._num_posts = X.shape[0]  # N
-        self._post_max_length = X.shape[1]  # L
-        self._post_embedding_dimension = X.shape[2]  # E
+        self._num_posts = X["rnn"].shape[0]  # N
+        self._num_dense_inputs = X["dense"].shape[1]
+        self._post_max_length = X["rnn"].shape[1]  # L
+        self._post_embedding_dimension = X["rnn"].shape[2]  # E
 
+        # set up RNN
         rnn_type = None
         if self.rnn_type is 'gru':
             rnn_type = tf.keras.layers.GRU
@@ -41,23 +44,31 @@ class AuthorClassifier(BaseEstimator, ClassifierMixin):
         else:
             assert False, "Unknwon RNN type"
 
-        inputs = tf.keras.Input(shape=(None, self._post_embedding_dimension))
+        rnn_input = tf.keras.Input(shape=(None, self._post_embedding_dimension))
 
-        masked = tf.keras.layers.Masking()(inputs)
-        rnn = rnn_type(self._num_users,
-                       # input_shape=(X.shape[1], self._num_words),
-                       return_sequences=(len(self.hidden_layer) > 0)
-                       )(masked)
+        masked = tf.keras.layers.Masking()(rnn_input)
+        rnn = rnn_type(self._num_users, return_sequences=(len(self.hidden_layer) > 0))(masked)
 
-        # TODO maybe add dropouts
+        if self.dropout > 0:
+            rnn = tf.keras.layers.Dropout(self.dropout)(rnn)
 
         for (i, hidden_neurons) in enumerate(self.hidden_layer):
             is_last_rnn_layer = i == len(self.hidden_layer) - 1
             rnn = rnn_type(hidden_neurons, return_sequences=not is_last_rnn_layer)(rnn)
 
-        output = tf.keras.layers.Dense(self._num_users, activation='softmax')(rnn)
+            if self.dropout > 0:
+                rnn = tf.keras.layers.Dropout(self.dropout)(rnn)
 
-        self._model = tf.keras.Model(inputs, output)
+        # dense inputs
+        dense_input = tf.keras.Input(shape=(self._num_dense_inputs))
+        # TODO probably need another layer here, but let's see
+        # output and model creation
+        concat = tf.keras.layers.concatenate([rnn, dense_input], axis=1)
+
+        output = tf.keras.layers.Dense(self._num_users, activation='softmax')(concat)
+
+        self._model = tf.keras.Model(inputs=[rnn_input, dense_input],
+                                     outputs=[output])
 
         self._model.compile(loss='categorical_crossentropy',
                             optimizer='adam',
@@ -66,7 +77,8 @@ class AuthorClassifier(BaseEstimator, ClassifierMixin):
         self._model.summary()
         tf.keras.utils.plot_model(self._model, "plots/aa-model.png", show_shapes=True, expand_nested=True)
 
-        self._model.fit(X, Y,
+        self._model.fit([X["rnn"], X["dense"]], Y,
+                        validation_data=([X_val["rnn"], X_val["dense"]], Y_val),
                         epochs=self.epochs,
                         batch_size=self.batch_size)
 
